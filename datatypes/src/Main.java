539186287753ffec795611e73abd8479f6b372b9
gizemgulsu <gizemgudzevic@gmail.com>
2022-10-14 00:39:55 +0300

    datatypes

---
+++ datatypes/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        double doubleSayi= 12.5;
+        int integerSayi =3;
+        integerSayi = 129;
+        byte sayi = 2;
+        char karakte ='A';
+        short kucukSayi =25;
+        long buyukSayi = 665444347;
+        String metin = "Mesaj";
+        boolean buyukMu =true;
+
+    }
+}
