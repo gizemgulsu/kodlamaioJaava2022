5f3dd08e4e59ca931c5fe864c36ad208ba86d127
gizemgulsu <gizemgudzevic@gmail.com>
2022-10-14 00:44:13 +0300

    variables

---
+++ variables/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Ogrenci sayisi = 10");
+        //
+        int sayi = 10;
+        System.out.println("Ogrenci sayisi " + sayi );
+        //
+        String mesaj = "Ogrenci sayisi : ";
+        System.out.println(mesaj + sayi);
+
+    }
+}
